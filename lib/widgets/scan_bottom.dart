3ebf92f9927f50d5456f492d4b9dd889fa80c0b5
denamorado <david8enaceituno@gmail.com>
2022-03-19 01:10:26 -0600

    se agrego el scan model

--- lib/widgets/scan_bottom.dart
+++ lib/widgets/scan_bottom.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanBottom extends StatelessWidget {
   const ScanBottom({Key? key}) : super(key: key);
@@ -7,8 +8,17 @@ class ScanBottom extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       //elevation: 00,
-      onPressed: () {}, 
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            "#ff6666", 
+            "Cancelar", 
+            false, 
+            ScanMode.QR
+        );
+        print(barcodeScanRes);
+      }, 
       child: const Icon(Icons.filter_center_focus)
+
       );
   }
 }
